2338491c0dfdf32e182f85069aa650cc34e22c64
ichg4354 <60804817+ichg4354@users.noreply.github.com>
2020-07-24 00:29:36 +0900

    finish routers basic connection path

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import routes from "./routes.js";
+import mainRouter from "./routers/mainRouter.js";
 
 const app = express();
 
+app.use(mainRouter);
 export default app;
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,2 +1,11 @@
-import routes from "../routes";
 import express from "express";
+import routes from "../routes.js";
+
+const mainRouter = express.Router();
+mainRouter.get(routes.MAIN, (req, res) => res.send("main"));
+mainRouter.get(routes.CUSTOMER, (req, res) => res.send("CUSTOMER"));
+mainRouter.get(routes.CEO, (req, res) => res.send("CEO"));
+mainRouter.get(routes.QPG1, (req, res) => res.send("QUESTION PAGE ONE"));
+mainRouter.get(routes.RESULTS, (req, res) => res.send("RESULTS"));
+
+export default mainRouter;
