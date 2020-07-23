032ae4e269fff373711345d61275fbf2fe059c19
ichg4354 <60804817+ichg4354@users.noreply.github.com>
2020-07-23 23:56:22 +0900

    create routes.js

---
+++ routes.js
@@ -0,0 +1,18 @@
+const main = "/reviewNow";
+const customer = "/:id/customer";
+const ceo = "/:id/ceo";
+const questionPage1 = "/questionPg1";
+const questionPage2 = "/questionPg2";
+const questionpage3 = "/questionPg3";
+const results = "results";
+
+const routes = {
+  MAIN: main,
+  CUSTOMER: customer,
+  CEO: ceo,
+  QPG1: questionPage1,
+  QPG2: questionPage2,
+  QPG3: questionpage3,
+  RESULTS: results,
+};
+export default routes;
