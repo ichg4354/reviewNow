af70271bc630553b41652f39bce91070a446a3e1
ichg4354 <60804817+ichg4354@users.noreply.github.com>
2020-07-24 09:57:21 +0900

    finish connection to pug to views

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ import express from "express";
 import routes from "./routes.js";
 import mainRouter from "./routers/mainRouter.js";
 
+import pug from "pug";
+
 const app = express();
+
+app.set("view engine", "pug");
 
 app.use(mainRouter);
 export default app;
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import routes from "../routes.js";
 
 const mainRouter = express.Router();
-mainRouter.get(routes.MAIN, (req, res) => res.send("main"));
+mainRouter.get(routes.MAIN, (req, res) => res.render("ceoResults"));
 mainRouter.get(routes.CUSTOMER, (req, res) => res.send("CUSTOMER"));
 mainRouter.get(routes.CEO, (req, res) => res.send("CEO"));
 mainRouter.get(routes.QPG1, (req, res) => res.send("QUESTION PAGE ONE"));
