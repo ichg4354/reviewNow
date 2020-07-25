d8e5e1881f91f0ea6bcf711c44fe8664f0699f0c
ichg4354 <60804817+ichg4354@users.noreply.github.com>
2020-07-25 20:07:21 +0900

    finish model create at getCeoCreate Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,25 @@
+import mongoose from "mongoose";
+import Ceo from "../models/Ceo.js";
+
 export const getCeoCreate = (req, res) => {
   res.render("ceoCreate");
 };
 
-export const postCeoCreate = (req, res) => {
+export const postCeoCreate = async (req, res) => {
+  const {
+    body: { nameOfBuisness, nameOfOwner, email, password1, password2 },
+  } = req;
+  try {
+    const ceo = await Ceo.create({
+      nameOfBuisness: nameOfOwner,
+      nameOfOwner: nameOfOwner,
+      email: email,
+    });
+    console.log(ceo);
+  } catch (e) {
+    console.log(e);
+  }
+  console.log(nameOfBuisness, nameOfOwner, email, password1, password2);
   res.send("post ceoCreate");
 };
 
